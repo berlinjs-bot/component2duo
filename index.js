2bdba55fb82f02ecb6b088b4460c92107eee9fc8
Anton Wilhelm <timaschew@gmail.com>
2014-08-19 14:17:02 +0200

    disable generator usage in index

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-module.exports = require('generator-supported')
-  ? require('./build')
-  : require('./build-generator');
+// disable this since, no genertor functions are used yet
+// module.exports = require('generator-supported')
+//   ? require('./build')
+//   : require('./build-generator');
+
+module.exports = require('./build');
